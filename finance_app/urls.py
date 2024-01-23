5ce95a071da4b535fe85fa85f1051608ddb9ada7
Jean Carlos R. Sousa <jeancarllos99@outlook.com>
2024-01-23 19:53:21 -0300

    feat(templates): adicionar urls, página principal e modais

--- finance_app/urls.py
+++ finance_app/urls.py
@@ -9,6 +9,16 @@ urlpatterns = [
     path('login', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
     path('register', views.register, name='register'),
-
-    
+    path('dashboard', views.DashboardApp.as_view(), name='dashboard'),
+    path('extract', views.ExtractApp.as_view(), name='extract'),
+    path('createearnings', views.create_earnings, name='createearnings'),
+    path('createexpenses', views.create_expenses, name='createexpenses'),
+    path('createcategoryearnings', views.create_category_earnings, name='createcategoryearnings'),
+    path('createcategoryexpenses', views.create_category_expenses, name='createcategoryexpenses'),
+    path('deletecategoryearnings', views.delete_category_earnings, name='deletecategoryearnings'),
+    path('deletecategoryexpenses', views.delete_category_expenses, name='deletecategoryexpenses'),
+    path('deleteearnings/<int:id>', views.delete_earnings, name='deleteearnings'),
+    path('deleteexpenses/<int:id>', views.delete_expenses, name='deleteexpenses'),
+    path('updateearnings/<int:id>', views.update_earnings, name='updateearnings'),
+    path('updateexpenses/<int:id>', views.update_expenses, name='updateexpenses'),
 ]
